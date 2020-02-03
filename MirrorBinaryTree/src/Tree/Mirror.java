577a0c5895a5822e13b9e7ab3244eb7c8b81f667
raserrock <piyushrajput032@gmail.com>
2020-02-03 10:19:34 +0530

    final commit

--- MirrorBinaryTree/src/Tree/Mirror.java
+++ MirrorBinaryTree/src/Tree/Mirror.java
@@ -1,11 +1,65 @@
 package Tree;
 
+class Node{
+	int data;
+	Node left,right;
+	public Node(int item) {
+		data=item;
+		left=right=null;
+	}
+}
+
 public class Mirror {
 
+	Node root;
+	void mirror() {
+		root=mirror(root);
+		
+	}
+	Node mirror(Node node) {
+		if(node == null)
+			return node;
+		//do the subtrees
+		Node left=mirror(node.left);
+		Node right=mirror(node.right);
+		
+		
+		//swapthe left and rightpointers
+		node.left=right;
+		node.right=left;
+		return node;
+	}
+	void inOrder() {
+		inOrder(root);
+	}
+	void inOrder(Node node) {
+		if(node==null)
+			return;
+		
+		inOrder(node.left);
+		System.out.print(node.data+" ");
+		
+		inOrder(node.right);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.print("he");
+		Mirror tree=new Mirror();
+		tree.root=new Node(1);
+		tree.root.left=new Node(2);
+		tree.root.right=new Node(3);
+		tree.root.left.left=new Node(4);
+		tree.root.left.right=new Node(5);
+		
+		//print inorder traversalof theinput tree
+		System.out.print("Inorder traversalofthe binary tre is");
+		tree.inOrder();
+		System.out.println();
+		
+		tree.mirror();
+		
+		System.out.println("Inorder traversalofthe binary tre is");
+		tree.inOrder();
 	}
 
 }
